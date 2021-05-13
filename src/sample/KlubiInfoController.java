86a231ce8d55f70378b5a05865316c4e5df9747f
KristoferArro <kristoferjohan16@gmail.com>
2021-05-13 13:49:41 +0300

    viimane commit

--- src/sample/KlubiInfoController.java
+++ src/sample/KlubiInfoController.java
@@ -1,13 +1,11 @@
 package sample;
 
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
--- src/sample/KlubivalikController.java
+++ src/sample/KlubivalikController.java
@@ -1,15 +1,11 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
-import javax.swing.*;
 import java.io.IOException;
 
 public class KlubivalikController {
